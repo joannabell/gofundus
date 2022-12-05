07303e7f947a42b08d8afd938a8d9e65fee4539e
Joanna Bell <jjbdigital@gmail.com>
2022-12-05 12:31:24 -0600

    nav bar styling

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import AuthenticationContext from './AuthenticationContext';
-import "./NavBar.css"
+import "./NavBar.css";
+import logo from "./assets/logo.jpg";
 
 function NavBar() {
     return (
         <AuthenticationContext.Consumer>
-            {({currentUser, isAuthenticated}) => (
+            {({ currentUser, isAuthenticated }) => (
                 <div className="navbar">
-                    <h1><a href="http://localhost:4000/">GoFundUs</a></h1>
+                    <button className="home-button"><a href="/">GoFundUs</a></button>
                     <div className="nav-links">
-                    { !isAuthenticated ? 
+                        {!isAuthenticated ?
                             <>
                                 <NavLink className="link" to="/register">Register</NavLink>
-                                <NavLink className="link" to="/login">Login</NavLink> 
+                                <NavLink className="link" to="/login">Login</NavLink>
                             </>
                             : null
-                    }
+                        }
                         <NavLink className="link" to="/sponsorships">Sponsorships</NavLink>
                     </div>
                 </div>
             )}
-            </AuthenticationContext.Consumer>
+        </AuthenticationContext.Consumer>
 
     )
 }
