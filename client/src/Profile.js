a150312c9a92de8733d0020026e88d3d0cc92d7b
Joanna Bell <jjbdigital@gmail.com>
2022-12-07 15:17:08 -0600

    updated Profile page styling

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import NavBar from "./NavBar"
 import AuthenticationContext from './AuthenticationContext';
+import "./Profile.css";
+import "./Sponsorship.css";
+import Sponsorship from "./Sponsorship";
+import Search from "./Search";
 
-function Profile() {
+function Profile( sponsorship ) {
+    const { id, name, needs, shelter } = sponsorship
+    const target = useRef(null);
+    const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+
+    function handleDeleteSponsorship(isAuthenticated) {
+        if (!isAuthenticated) {
+            setShow(true);
+        } else {
+            fetch("/signups", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    sponsorship_id: id,
+                    user_id: id
+                }),
+            })
+                .then((r) => r.json())
+                .then((data) => console.log(data))
+        }
+    }
+
     return (
         <AuthenticationContext.Consumer>
             {({ isAuthenticated, currentUser }) => (
-                <div className="profile-container">
+                <div className="profile-page">
+                    <NavBar />
                     <div>
                         <h2>Welcome, {currentUser.name}</h2>
-                        
+                    </div>
+                    <div>
+                        <div className="sponsorship-search">
+                            <Search handleSearchChange={setSearchValue} searchValue={searchValue} />
+                        </div>
+                    </div>
+                    <div className="profile-container">
+                        <div className="sponsorship-card">
+                            <div className="sponsorship">
+                                <p>Shelter: <span>{shelter}</span></p>
+                                <p>Needs: <span>{needs}</span> </p>
+                                <button ref={target} className="button-6" onClick={() => handleDeleteSponsorship(isAuthenticated)}>-</button>
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
             )}
