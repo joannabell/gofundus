39d7cc51c5c16c0b55e56ba5ff55f93daf1406bb
Joanna Bell <jjbdigital@gmail.com>
2022-12-08 13:45:00 -0600

    commented out skip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user, only: [:create, :show]
+#   skip_before_action :authenticate_user, only: [:create, :show]
   
   def index 
       users = User.all 
