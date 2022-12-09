663ff8b30b1d1bd0aab830ff1f8c72e7af843723
Joanna Bell <jjbdigital@gmail.com>
2022-12-08 19:42:32 -0600

    fetched the /me page

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import NavBar from "./NavBar"
 import AuthenticationContext from './AuthenticationContext';
 import "./Profile.css";
@@ -6,30 +6,36 @@ import "./Sponsorship.css";
 import Sponsorship from "./Sponsorship";
 import Search from "./Search";
 
-function Profile( sponsorship ) {
-    const { id, name, needs, shelter } = sponsorship
+function Profile(user) {
+    // const { id, name, needs, shelter } = sponsorship
     const target = useRef(null);
     const [show, setShow] = useState(false);
     const [searchValue, setSearchValue] = useState("");
+    const [signups, setSignups] = useState([]);
 
-    function handleDeleteSponsorship(isAuthenticated) {
-        if (!isAuthenticated) {
-            setShow(true);
-        } else {
-            fetch("/signups", {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    sponsorship_id: id,
-                    user_id: id
-                }),
-            })
-                .then((r) => r.json())
-                .then((data) => console.log(data))
-        }
-    }
+    useEffect(() => {
+        fetch("/me").then((r) => r.json()).then((data) => console.log(data))
+    })
+
+    // function handleDeleteSponsorship(isAuthenticated) {
+
+    //     if (!isAuthenticated) {
+    //         setShow(true);
+    //     } else {
+    //         fetch("/signups", {
+    //             method: "DELETE",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({
+    //                 sponsorship_id: id,
+    //                 user_id: user.id
+    //             }),
+    //         })
+    //             .then((r) => r.json())
+    //             .then((data) => console.log(data))
+    //     }
+    // }
 
     return (
         <AuthenticationContext.Consumer>
@@ -37,7 +43,7 @@ function Profile( sponsorship ) {
                 <div className="profile-page">
                     <NavBar />
                     <div>
-                        <h2>Welcome, {currentUser.name}</h2>
+                        <h2>Welcome, {user.name}</h2>
                     </div>
                     <div>
                         <div className="sponsorship-search">
@@ -45,21 +51,18 @@ function Profile( sponsorship ) {
                         </div>
                     </div>
                     <div className="profile-container">
-                        <div className="sponsorship-card">
+                        {/* <div className="sponsorship-card">
                             <div className="sponsorship">
                                 <p>Shelter: <span>{shelter}</span></p>
                                 <p>Needs: <span>{needs}</span> </p>
                                 <button ref={target} className="button-6" onClick={() => handleDeleteSponsorship(isAuthenticated)}>-</button>
                             </div>
-                        </div>
-
-
+                        </div> */}
                     </div>
                 </div>
             )}
-
         </AuthenticationContext.Consumer>
-    )
+    );
 }
 
 export default Profile;
