59501607ceb2e9d1b0f67bba7b7c5459ee40532a
Joanna Bell <jjbdigital@gmail.com>
2022-12-08 19:37:47 -0600

    created method for sponsorships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,52 @@
 class UsersController < ApplicationController
-#   skip_before_action :authenticate_user, only: [:create, :show]
-  
-  def index 
-      users = User.all 
-      render json: users, each_serializer: UserSerializer
+  #   skip_before_action :authenticate_user, only: [:create, :show]
+
+  def index
+    users = User.all
+    render json: users, each_serializer: UserSerializer
   end
 
-  def show 
-      if current_user
-          render json: current_user, status: :ok
-      else 
-          render json: "Not authenticated", status: :unauthorized
-      end 
+  def show
+    user = User.find(params[:id])
+
+    if user
+      render json: user
+    else
+      render json: { error: "User not found" }, status: 404
+    end
   end
 
-  def create 
-      user = User.create(user_params)
-      if user.valid?
-          session[:user_id] = user.id
-          render json: user, status: :created 
-      else 
-          render json: user.errors.full_messages, status: :unprocessable_entity 
-      end 
+  def me
+    if current_user
+      render json: current_user, status: :ok
+    else
+      render json: "Not authenticated", status: :unauthorized
+    end
   end
 
-  def destroy 
-      user = User.find_by(id: params[:id])
-      if user 
-          user.destroy
-          head :no_content 
-      else  
-          render json: "User does not exist", status: :not_found 
-      end
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
-  private 
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user
+      user.destroy
+      head :no_content
+    else
+      render json: "User does not exist", status: :not_found
+    end
+  end
 
-  def user_params 
-      params.permit(:email, :password, :password_confirmation)
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
